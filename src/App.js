cb7bcc0ec48dc3a1acdd132ab8ef6a1c3b6ba8b6
Raunak <reachtoraunak@gmail.com>
2020-07-26 22:23:28 +0530

    Blog.js as layout :construction:

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
+import Blog from "./containers/Blog/Blog";
 
 class App extends Component {
   render() {
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        <Blog />
+      </div>
+    );
   }
 }
 
---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Blog extends Component {
+  render() {
+    return (
+      <div>
+        <section>Post component</section>
+        <section>FullPost component</section>
+        <section>NewPost component</section>
+      </div>
+    );
+  }
+}
+
+export default Blog;
