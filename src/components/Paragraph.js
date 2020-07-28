656bcd5feffe0fb0898baa8486db166a40653749
Raunak <reachtoraunak@gmail.com>
2020-07-28 14:07:00 +0530

    Adding Paragraph and Text component :sparkles:

---
+++ src/components/Paragraph.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Box } from "rebass";
+import styled from "styled-components";
+import Text from "./Text";
+
+const Wrapper = styled(Box)`
+  max-wdith: 35em;
+  width: 100%;
+  position: relative;
+  z-index: 1;
+  ${(props) => props.centered && `margin:0 auto`}
+`;
+
+export default ({ centered, as, ...props }) => (
+  <Wrapper centered={centered} as={as}>
+    <Text color="#333" mb={3} fontSize="18px" lineHeight={1.6} {...props} />
+  </Wrapper>
+);
---
+++ src/components/Text.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { Text } from "rebass";
+
+export default (props) => (
+  <Text fontFamily="system" as="p" color="text" {...props} />
+);
