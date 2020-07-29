00e0eb591de51d210e32ac3a79a7390b0bdaf5b4
Raunak <reachtoraunak@gmail.com>
2020-07-29 21:15:14 +0530

    Bugfix in CardGrid.js :bug:

--- src/components/CardGrid.js
+++ src/components/CardGrid.js
@@ -1,5 +1,5 @@
 import React from "react";
 import { Flex } from "rebass";
 export default (props) => (
-  <Flex flexDirection="row" flexWrap="wrap" m={[1, 2]} {...props} />
+  <Flex flexDirection="row" flexWrap="wrap" m={[-1, -2]} {...props} />
 );
