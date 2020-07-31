40155fc173e740b2398bb841ccb4ca7fe708603e
Raunak <reachtoraunak@gmail.com>
2020-07-31 13:47:24 +0530

    NewPost.js alignment and styled components improvements :construction:

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -2,15 +2,12 @@ import React, { Component } from "react";
 import axios from "axios";
 import Button from "../Button";
 import WideSection from "../WideSection";
-import Item from "../Item";
 import Card from "../Card";
 
 import styled from "styled-components";
 import { Box } from "rebass";
-import { Select } from "@rebass/forms";
-import thumbnailUrl from "../../thumbnail.jpg";
 
-const StyledForm = styled.div.attrs({
+const FormLayout = styled.div.attrs({
   as: "div",
 })`
   display: flex;
@@ -28,9 +25,8 @@ const Label = styled(Box).attrs({
 })`
   font-size: 1em;
   border-radius: 3px;
-  width: 20em;
+  text-align: justify;
   padding: 0.5em;
-  display: block;
 `;
 
 const InputBox = styled(Box).attrs({
@@ -41,9 +37,44 @@ const InputBox = styled(Box).attrs({
 })`
   font-size: 1em;
   border-radius: 3px;
-  width: 20em;
+  width: 80%;
   border: 1px solid ${(props) => props.theme.colors.greys[1]};
   padding: 0.5em;
+  display: block;
+  :hover {
+    border: 1px solid ${(props) => props.theme.colors.greys[3]};
+  }
+`;
+
+const TextAreaBox = styled(Box).attrs({
+  type: "textarea",
+  as: "textarea",
+  my: 2,
+  mr: 2,
+})`
+  font-size: 1em;
+  border-radius: 3px;
+  width: 80%;
+  border: 1px solid ${(props) => props.theme.colors.greys[1]};
+  padding: 0.5em;
+  display: block;
+  :hover {
+    border: 1px solid ${(props) => props.theme.colors.greys[3]};
+  }
+`;
+
+const SelectBox = styled(Box).attrs({
+  type: "select",
+  as: "select",
+  my: 2,
+  mr: 2,
+})`
+  font-size: 1em;
+  border-radius: 3px;
+  width: 80%;
+  border: 1px solid ${(props) => props.theme.colors.greys[1]};
+  padding: 0.5em;
+  display: block;
   :hover {
     border: 1px solid ${(props) => props.theme.colors.greys[3]};
   }
@@ -71,47 +102,33 @@ class NewPost extends Component {
     return (
       <WideSection>
         <Card hover={false} my={4}>
-          <Card.Title css="margin-top:0">Add a Post</Card.Title>
-          <StyledForm>
+          <Card.Title>Add a Post</Card.Title>
+          <FormLayout>
             <Label p={1}>Title</Label>
             <InputBox
               p={2}
-              type="text"
               value={this.state.title}
               onChange={(event) => this.setState({ title: event.target.value })}
             />
             <Label p={1}>Content</Label>
-            <InputBox
+            <TextAreaBox
               p={2}
-              type="textarea"
               value={this.state.content}
               onChange={(event) =>
                 this.setState({ content: event.target.value })
               }
             />
-
             <Label p={1}>Author</Label>
-            <Select
-              p={2}
-              width="250px"
-              value={this.state.author}
-              onChange={(event) => this.setState({ title: event.target.value })}
-            >
+            <SelectBox>
               <option value="Raunak">Raunak</option>
               <option value="Max">Max</option>
-            </Select>
+            </SelectBox>
 
             <Card.Body>
               <Button onClick={this.postDataHandler}>Add Post</Button>
             </Card.Body>
-          </StyledForm>
+          </FormLayout>
         </Card>
-
-        <Item
-          title="Hello"
-          subtitle="can you hear me"
-          imageUrl={thumbnailUrl}
-        />
       </WideSection>
     );
   }
